81c90ec08533b5231f4084698f8ae23f34f38da2
Parralol <santinico36@hotmail.com>
2024-07-21 15:32:49 -0500

    FULLY FUNCTION

--- src/InvadersGame.js
+++ src/InvadersGame.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
 const InvadersGame = () => {
@@ -6,7 +6,7 @@ const InvadersGame = () => {
     const [players, setPlayers] = useState({});
     const [playerId, setPlayerId] = useState(null);
     const [ws, setWs] = useState(null);
-    const [playerName, setPlayerName] = useState(''); // State for player name
+    const [playerName, setPlayerName] = useState('');
     const canvasRef = useRef(null);
     const playerInfoRef = useRef(null);
 
@@ -23,27 +23,31 @@ const InvadersGame = () => {
     });
 
     useEffect(() => {
-        // Load all images
-        images.current.monster[0].src = 'resources/bicho.gif';  // Update paths as needed
-        images.current.monster[1].src = 'resources/bicho1.gif';
-        
-        images.current.crab[0].src = 'resources/Crab.gif';
-        images.current.crab[1].src = 'resources/Crab1.gif';
+        const loadImage = (src, callback) => {
+            const img = new Image();
+            img.src = src;
+            img.onload = callback;
+            img.onerror = () => console.error(`Failed to load image at ${src}`);
+            return img;
+        };
 
-        images.current.laser[0].src = 'resources/laser.gif';
-        images.current.laser[1].src = 'resources/laser2.gif';
+        images.current.monster[0] = loadImage('resources/bicho.gif', () => console.log('Monster image 0 loaded'));
+        images.current.monster[1] = loadImage('resources/bicho1.gif', () => console.log('Monster image 1 loaded'));
 
-        images.current.bullet[0].src = 'resources/misil.gif';
-        images.current.bullet[1].src ='resources/misil.gif';
+        images.current.crab[0] = loadImage('resources/Crab.gif', () => console.log('Crab image 0 loaded'));
+        images.current.crab[1] = loadImage('resources/Crab1.gif', () => console.log('Crab image 1 loaded'));
 
-        images.current.bomb[0].src ='resources/bombD.gif';
-        images.current.bomb[1].src ='resources/bombDR.gif';
+        images.current.laser[0] = loadImage('resources/laser.gif', () => console.log('Laser image 0 loaded'));
+        images.current.laser[1] = loadImage('resources/laser2.gif', () => console.log('Laser image 1 loaded'));
 
-        // Load the ship image
-        shipImage.current.src = 'resources/ship.gif'; // Update with the correct path to your image
-        shipImage.current.onload = () => console.log('Ship image loaded');
+        images.current.bullet[0] = loadImage('resources/misil.gif', () => console.log('Bullet image 0 loaded'));
+        images.current.bullet[1] = loadImage('resources/misil.gif', () => console.log('Bullet image 1 loaded'));
 
-        // Connect to the WebSocket server
+        images.current.bomb[0] = loadImage('resources/bombD.gif', () => console.log('Bomb image 0 loaded'));
+        images.current.bomb[1] = loadImage('resources/bombDR.gif', () => console.log('Bomb image 1 loaded'));
+
+        shipImage.current = loadImage('resources/ship.gif', () => console.log('Ship image loaded'));
+
         const socket = new ReconnectingWebSocket(`ws://${host}/ws/game`);
         setWs(socket);
 
@@ -56,8 +60,13 @@ const InvadersGame = () => {
             console.log('Received message:', message);
 
             try {
-                const json = JSON.parse(message);
-                setPlayers(json);
+                // Try parsing as players JSON
+                const data = JSON.parse(message);
+                if (data && data[Object.keys(data)[0]].name) {
+                    setPlayers(data);
+                } else {
+                    setEntities(data);
+                }
             } catch (e) {
                 console.error('Failed to parse JSON:', e);
                 console.log('Message content:', message);
@@ -77,60 +86,124 @@ const InvadersGame = () => {
         };
     }, [host]);
 
-    useEffect(() => {
-        const drawPlayers = (ctx) => {
-            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-            Object.values(players).forEach(player => {
-                ctx.drawImage(shipImage.current, player.x, player.y);
-            });
-        };
+    const drawEntities = useCallback((ctx) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.fillStyle = 'black'; // Set background color
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Fill the canvas background with black
 
-        const updateCanvas = () => {
-            const canvas = canvasRef.current;
-            if (canvas) {
-                const ctx = canvas.getContext('2d');
-                if (ctx) {
-                    drawPlayers(ctx);
-                }
+        Object.values(entities).forEach(entity => {
+            let image;
+            switch (entity.type) {
+                case 'Monster':
+                    image = images.current.monster[0]; // Adjust as needed
+                    break;
+                case 'Crab':
+                    image = images.current.crab[0]; // Adjust as needed
+                    break;
+                case 'Laser':
+                    image = images.current.laser[0]; // Adjust as needed
+                    break;
+                case 'Bullet':
+                    image = images.current.bullet[0]; // Adjust as needed
+                    break;
+                case 'Bomb':
+                    image = images.current.bomb[0]; // Adjust as needed
+                    break;
+                default:
+                    console.log('Unknown entity type:', entity.type);
+                    return; // Skip unknown types
             }
-        };
 
+            if (image.complete) {
+                ctx.drawImage(image, entity.x, entity.y);
+            } else {
+                console.log(`Image for type ${entity.type} not loaded`);
+            }
+        });
+
+        // Draw players separately
+        Object.values(players).forEach(player => {
+            if (shipImage.current.complete) {
+                ctx.drawImage(shipImage.current, player.x, player.y);
+
+                // Draw player life bar
+                const maxLife = 200; // Adjust according to your maximum life value
+                const barWidth = 50;
+                const barHeight = 10;
+                const lifeWidth = (player.life / maxLife) * barWidth;
+                const infoStyle = {
+                    position: 'absolute',
+                    left: `${player.x}px`,
+                    top: `${player.y - 30}px`, // Position above player
+                    color: 'white',
+                    fontFamily: 'Arial, sans-serif',
+                    fontSize: '12px',
+                    backgroundColor: 'black',
+                    padding: '2px',
+                    borderRadius: '4px',
+                };
+
+                // Draw life bar background
+                ctx.fillStyle = 'red';
+                ctx.fillRect(player.x, player.y - 20, barWidth, barHeight);
+                
+                // Draw life bar foreground
+                ctx.fillStyle = 'green';
+                ctx.fillRect(player.x, player.y - 20, lifeWidth, barHeight);
+
+                // Draw player life text
+                ctx.fillStyle = 'white';
+                ctx.font = '12px Arial';
+                ctx.fillText(`${player.life}`, player.x + barWidth / 2 - 10, player.y - 10);
+            } else {
+                console.log('Ship image not loaded');
+            }
+        });
+    }, [entities, players]);
+
+    const updateCanvas = useCallback(() => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                drawEntities(ctx);
+            } else {
+                console.log('Context not found');
+            }
+        } else {
+            console.log('Canvas not found');
+        }
+    }, [drawEntities]);
+
+    useEffect(() => {
         const intervalId = setInterval(updateCanvas, 50); // Update every 50 ms
 
         return () => {
             clearInterval(intervalId);
         };
-    }, [players]);
+    }, [updateCanvas]);
 
-    const handleKeyDown = (event) => {
-        console.log('Key down event:', event);
+    const handleKeyDown = useCallback((event) => {
         if (ws && playerId) {
             const message = JSON.stringify({
                 type: 'keydown',
                 keyCode: event.keyCode,
                 playerId,
             });
-            console.log('Sending message:', message);
             ws.send(message);
-        } else {
-            console.log('WebSocket or Player ID not set.');
         }
-    };
+    }, [ws, playerId]);
 
-    const handleKeyUp = (event) => {
-        console.log('Key up event:', event);
+    const handleKeyUp = useCallback((event) => {
         if (ws && playerId) {
             const message = JSON.stringify({
                 type: 'keyup',
                 keyCode: event.keyCode,
                 playerId,
             });
-            console.log('Sending message:', message);
             ws.send(message);
-        } else {
-            console.log('WebSocket or Player ID not set.');
         }
-    };
+    }, [ws, playerId]);
 
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
@@ -140,7 +213,7 @@ const InvadersGame = () => {
             window.removeEventListener('keydown', handleKeyDown);
             window.removeEventListener('keyup', handleKeyUp);
         };
-    }, [ws, playerId]);
+    }, [handleKeyDown, handleKeyUp]);
 
     const handleNameChange = (event) => {
         setPlayerName(event.target.value);
@@ -153,7 +226,6 @@ const InvadersGame = () => {
                 playerId,
                 name: playerName,
             });
-            console.log('Sending name change message:', message);
             ws.send(message);
         }
     };
@@ -172,49 +244,25 @@ const InvadersGame = () => {
         const canvas = canvasRef.current;
         if (!canvas) return null;
 
-        // Get canvas position and dimensions
         const canvasRect = canvas.getBoundingClientRect();
 
         return Object.values(players).map((player) => {
-            const { x, y, name, life } = player;
-            const maxLife = 100; // Adjust according to your maximum life value
+            const { x, y, name } = player;
             const infoStyle = {
                 position: 'absolute',
                 left: `${canvasRect.left + x}px`,
-                top: `${canvasRect.top + y - 60}px`, // Position above player
-                color: 'black',
+                top: `${canvasRect.top + y - 60}px`,
+                color: 'white',
                 fontFamily: 'Arial, sans-serif',
                 fontSize: '12px',
-                backgroundColor: 'white', // Ensure visibility
+                backgroundColor: 'black',
                 padding: '2px',
                 borderRadius: '4px',
             };
-            const barWidth = 50;
-            const barHeight = 10;
-            const lifeWidth = (life / maxLife) * barWidth;
 
             return (
                 <div key={name} style={infoStyle}>
                     <div>{name}</div>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <div
-                            style={{
-                                backgroundColor: 'red',
-                                width: barWidth,
-                                height: barHeight,
-                                marginRight: '5px',
-                            }}
-                        >
-                            <div
-                                style={{
-                                    backgroundColor: 'green',
-                                    width: lifeWidth,
-                                    height: barHeight,
-                                }}
-                            />
-                        </div>
-                        <div>{life} Life</div>
-                    </div>
                 </div>
             );
         });
@@ -223,7 +271,7 @@ const InvadersGame = () => {
     return (
         <div>
             <h1>Invaders Game</h1>
-            <canvas ref={canvasRef} width={800} height={600} style={{ border: '1px solid black' }}></canvas>
+            <canvas ref={canvasRef} width={640} height={480} style={{ border: '1px solid black' }}></canvas>
             <div ref={playerInfoRef}>
                 {renderPlayerInfo()}
             </div>
@@ -235,8 +283,8 @@ const InvadersGame = () => {
                             type="text"
                             value={playerName}
                             onChange={handleNameChange}
-                            onBlur={handleNameBlur} // Send name on blur
-                            onKeyPress={handleKeyPress} // Send name on Enter key
+                            onBlur={handleNameBlur}
+                            onKeyPress={handleKeyPress}
                             placeholder="Enter new name"
                         />
                     </label>
